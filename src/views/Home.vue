24867a835a3062380d0c4fb617a48ba4e6533e17
DoomSky <by_mori@163.com>
2021-08-29 17:06:11 +0800

    iconfont图标引用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,16 @@
  * @Author: by_mori
  * @Date: 2021-08-29 15:21:41
  * @LastEditors: by_mori
- * @LastEditTime: 2021-08-29 16:45:51
+ * @LastEditTime: 2021-08-29 17:03:45
 -->
 <template>
   <div class="home">
-    <div id="d1"></div>
+    <div id="d1">
+      <svg class="icon" aria-hidden="true">
+    <use xlink:href="#icon-sousuo1"></use>
+</svg>
+      <span class="iconfont icon-sousuo1"></span>
+    </div>
   </div>
 </template>
 
