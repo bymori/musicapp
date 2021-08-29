6a1e5763af5b82d71a9fba55724e842898237c4e
DoomSky <by_mori@163.com>
2021-08-29 17:57:01 +0800

    头部导航布局和样式

---
+++ src/components/topNav.vue
@@ -0,0 +1,61 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: by_mori
+ * @Date: 2021-08-29 17:32:14
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-08-29 17:54:43
+-->
+<template >
+  <div class="topNav">
+    <div class="topLeft">
+      <svg class="icon"
+           aria-hidden="true">
+        <use xlink:href="#icon-gengduo1"></use>
+      </svg>
+    </div>
+    <div class="topCenter">
+      <span class="navBtn">我的</span>
+      <span class="navBtn action">发现</span>
+      <span class="navBtn">云村</span>
+      <span class="navBtn">视频</span>
+    </div>
+    <div class="topRight">
+      <svg class="icon sousuo"
+           aria-hidden="true">
+        <use xlink:href="#icon-sousuo"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="less" scoped>
+.topNav {
+  width: 7.5rem;
+  height: 0.8rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  // padding: 0 0.2rem;
+  .icon {
+    width: 0.5rem;
+    height: 0.5rem;
+  }
+  .sousuo {
+    width: 0.45rem;
+    height: 0.45rem;
+  }
+  .topCenter {
+    width: 5rem;
+    display: flex;
+    justify-content: space-around;
+    .action{
+        font-weight: 900;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,35 +4,27 @@
  * @Author: by_mori
  * @Date: 2021-08-29 15:21:41
  * @LastEditors: by_mori
- * @LastEditTime: 2021-08-29 17:03:45
+ * @LastEditTime: 2021-08-29 17:48:34
 -->
 <template>
   <div class="home">
-    <div id="d1">
-      <svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-sousuo1"></use>
-</svg>
-      <span class="iconfont icon-sousuo1"></span>
-    </div>
+    <top-nav></top-nav>
   </div>
 </template>
-
+  
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import TopNav from '@/components/topNav.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TopNav
   }
 }
 </script>
 
 <style lang="less" scoped>
-#d1{
-  width: 3.75rem;
-  height: 3.75rem;
-  background: skyblue;
-}
+
 </style>
