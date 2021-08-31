354b8cdd296412eed4a5e5e50bf56449582422ca
DoomSky <by_mori@163.com>
2021-08-31 16:11:33 +0800

    图标导航区域

---
+++ src/components/iconList.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: by_mori
+ * @Date: 2021-08-31 15:33:30
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-08-31 16:10:22
+-->
+<template>
+  <div class="iconList">
+    <div class="iconItem">
+      <svg class="icon"
+           aria-hidden="true">
+        <use xlink:href="#icon-tuijian"></use>
+      </svg>
+      <span>每日推荐</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon"
+           aria-hidden="true">
+        <use xlink:href="#icon-FM_weixuanze"></use>
+      </svg>
+      <span>私人FM</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon"
+           aria-hidden="true">
+        <use xlink:href="#icon-gedan"></use>
+      </svg>
+      <span>歌单</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon"
+           aria-hidden="true">
+        <use xlink:href="#icon-paihangbang"></use>
+      </svg>
+      <span>排行榜</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iconList'
+}
+</script>
+
+<style lang="less" scoped>
+.iconList {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.4rem;
+  
+  .iconItem {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon {
+      width: 1rem;
+      height: 1rem;
+    }
+    span{
+        font-size: 0.26rem;
+    }
+  }
+}
+</style>>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,13 @@
  * @Author: by_mori
  * @Date: 2021-08-29 15:21:41
  * @LastEditors: by_mori
- * @LastEditTime: 2021-08-31 13:59:15
+ * @LastEditTime: 2021-08-31 15:36:51
 -->
 <template>
   <div class="home">
     <top-nav></top-nav>
     <Banner></Banner>
+    <IconList></IconList>
   </div>
 
 </template>
@@ -17,11 +18,12 @@
 <script>
 import TopNav from '@/components/topNav.vue'
 import Banner from '@/components/Banner.vue'
+import IconList from '@/components/iconList.vue'
 export default {
   name: 'Home',
   components: {
     TopNav,
-    Banner
+    Banner, IconList
   },
 
 }
