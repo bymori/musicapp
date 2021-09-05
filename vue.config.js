c08d4b06339349de83e4963e874e666891be407b
DoomSky <by_mori@163.com>
2021-09-05 17:22:40 +0800

    打包配置 初步目标实现

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-09-05 17:13:36
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-09-05 17:19:18
+ */
+module.exports = {
+    pages:{
+        index:{
+            entry:"src/main.js",
+            title:"沫沫音乐"
+        }
+    }
+};
